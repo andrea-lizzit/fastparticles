411eccdf65bf4fbb4e207249a3cc4991bb21d55d
Andrea Lizzit <andrea.lizzit@gmail.com>
2023-06-26 23:07:26 +0200

    Oganesyan and Huse sampler

--- simulation/operators.py
+++ simulation/operators.py
@@ -1,8 +1,10 @@
 import numpy as np
+import cupy as cp
 import math
 from copy import copy
 from collections import namedtuple
-
+import itertools
+from functools import cache
 
 class SystemSpec:
     def __init__(self, n, e):
@@ -12,33 +14,62 @@ class SystemSpec:
 
     def remake(self, **kwargs):
         spec = copy(self)
-        for key, value in kwargs:
+        for key, value in kwargs.items():
             setattr(spec, key, value)
         return spec
 
-def indexof(qubits, systemspec):
-    """ Index in the basis of the state with excited qubits in the given positions. """
-    if not sorted(excitations):
-        raise ValueError("Array is not sorted")
-    if len(qubits) != systemspec.e:
+@cache
+def indexofr(qubits, n, e):
+    """ Index in the basis of the state with excited qubits in the given positions. Recursive implementation: faster, but might run out of cache. """
+    # disable when not debugging
+    # it is a relatively expensive check in a time-critical piece of code
+    # if not np.all(np.diff(qubits) >= 0):
+    #     raise ValueError("Array is not sorted")
+    if len(qubits) != e:
         raise ValueError("Wrong number of excited qubits")
-    if systemspec.n == systemspec.e:
+    if n == e:
         return 0
+    if e == 1:
+        return qubits[0]
     k = qubits[0]
-    count = math.comb(systemspec.n, systemspec.e) - math.comb(systemspec.n - k, systemspec.e)
-    return count + indexof(qubits[1:], systemspec.remake(n=systemspec.n-1))
+    count = math.comb(n, e) - math.comb(n - k, e)
+    newqubits = tuple(q-k-1 for q in qubits[1:])
+    return count + indexof(newqubits, n-k-1, e-1)
+
+@cache
+def indexof(qubits, n, e):
+    """ Index in the basis of the state with excited qubits in the given positions. """
+    # disable when not debugging
+    # it is a relatively expensive check in a time-critical piece of code
+    # if not np.all(np.diff(qubits) >= 0):
+    #     raise ValueError("Array is not sorted")
+    count = 0
+    while True:
+        if len(qubits) != e:
+            raise ValueError("Wrong number of excited qubits")
+        if n == e:
+            return count
+        if e == 1:
+            return count + qubits[0]
+        k = qubits[0]
+        count += math.comb(n, e) - math.comb(n - k, e)
+        qubits = tuple(q-k-1 for q in qubits[1:])
+        n = n-k-1
+        e = e-1
 
     
 def exchange(i, j, systemspec):
     """ Representation of the a_i^\dagger*a_j operator in an n-qubit system in the e-particle basis. """
-    if e > n:
+    if systemspec.e > systemspec.n:
         raise ValueError("The number of particles cannot be higher than the number of qubits")
-    mat = np.zeros((systemspec.N, systemspec.N))
-    for index in np.ndindex(*[systemspec.n]*(systemspec.e-1)):
-        if np.any(index == i) or np.any(index == j):
-            continue
-        before = sort(np.insert(index, e, j))
-        after = sort(np.insert(index, e, i))
+    mat = cp.zeros((systemspec.N, systemspec.N))
+    available = list(range(systemspec.n))
+    available.remove(i)
+    if i != j:
+        available.remove(j)
+    for index in itertools.combinations(available, systemspec.e-1):
+        before = indexof(tuple(sorted(index + (j,))), systemspec.n, systemspec.e)
+        after = indexof(tuple(sorted(index + (i,))), systemspec.n, systemspec.e)
         mat[after, before] = 1
     return mat
     
--- simulation/tests.py
+++ simulation/tests.py
@@ -1,7 +1,11 @@
 import unittest
 import numpy as np
 from studies import ManyBodyLevels
-from matrixensembles import MatrixStats
+from matrixensembles import MatrixStats, OganesyanHuseSampler
+from operators import SystemSpec, indexof, exchange
+import itertools
+import random
+
 
 def simplestats(r=0):
 	stats = MatrixStats(None)
@@ -10,6 +14,7 @@ def simplestats(r=0):
 	arr += rng.normal(scale=r, size=arr.shape)
 	stats.eigenvalues_ = arr
 	return stats
+
 
 class TestManyBody(unittest.TestCase):
     
@@ -32,6 +37,65 @@ class TestManyBody(unittest.TestCase):
 				idx += 1
 		mbe = np.sort(mbe, axis=1)
 		self.assertIsNone(np.testing.assert_array_equal(mbe, mbstats.eigenvalues()))
+
+
+class TestSpins(unittest.TestCase):
+
+	def test_basis_index(self):
+		systemspec = SystemSpec(4, 2)
+		self.assertEqual(indexof((0, 1), systemspec.n, systemspec.e), 0)
+		self.assertEqual(indexof((0, 2), systemspec.n, systemspec.e), 1)
+		self.assertEqual(indexof((1, 3), systemspec.n, systemspec.e), 4)
+
+		systemspec = SystemSpec(5, 4)
+		for i, c in enumerate(itertools.combinations(range(systemspec.n), systemspec.e)):
+			self.assertEqual(indexof(c, systemspec.n, systemspec.e), i)
+
+		systemspec = SystemSpec(6, 3)
+		for i, c in enumerate(itertools.combinations(range(systemspec.n), systemspec.e)):
+			self.assertEqual(indexof(c, systemspec.n, systemspec.e), i)
+
+	def test_exchange(self):
+		systemspec = SystemSpec(10, 3)
+		mat = exchange(9, 6, systemspec).get()
+		v, w = np.zeros((systemspec.N,)), np.zeros((systemspec.N,))
+		v[indexof((3,4,6), systemspec.n, systemspec.e)] = 1
+		w[indexof((3,4,9), systemspec.n, systemspec.e)] = 1
+		self.assertIsNone(np.testing.assert_array_equal(mat@v, w))
+
+	def test_exchange_auto(self):
+		systemspec = SystemSpec(10, 3)
+		for i, j in np.ndindex(systemspec.n, systemspec.n):
+			mat = exchange(i, j, systemspec).get()
+			choices = list(range(systemspec.n))
+			choices.remove(i)
+			if i != j:
+				choices.remove(j)
+			k, l = random.sample(choices, k=2)
+			v, w = np.zeros((systemspec.N,)), np.zeros((systemspec.N,))
+			v[indexof(tuple(sorted((j, k, l))), systemspec.n, systemspec.e)] = 1
+			w[indexof(tuple(sorted((i, k, l))), systemspec.n, systemspec.e)] = 1
+			self.assertIsNone(np.testing.assert_array_equal(mat@v, w))
+
+
+class TestSampler(unittest.TestCase):
+
+	def test_oganesyanhusesampler(self):
+		n, e = 13, 2
+		sampler = OganesyanHuseSampler(n, 0, e=e)
+		systemspec = SystemSpec(n, e)
+		H = sampler.sample().get()
+		for m in range(n):
+			m1 = (m+1) % n
+			if m1 < m:
+				m, m1 = m1, m
+			with self.subTest(m=m, m1=m1):
+				state = np.zeros((systemspec.N,))
+				state[indexof((m, m1), systemspec.n, systemspec.e)] = 1
+				exp = state @ H @ state
+				reference = sampler.V*(0.25*systemspec.n-1)
+				self.assertAlmostEqual(exp, reference, delta=1e-10)
+
 
 if __name__=="__main__":
 	unittest.main()
